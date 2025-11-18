c9be7b1ef3dc6d6fdc83f2980a7c8bfa2a356e67
신동욱 <ehd2610@naver.com>
2025-11-18 18:43:00 +0900

    refactor: 디버깅

--- nodes/control/control_node.py
+++ nodes/control/control_node.py
@@ -24,19 +24,19 @@ def tv_channel(value):
     print("channel 변경: ", value)
 
 DEVICE_NORMALIZE = {
-    "air_conditioner": ["에어컨", "aircon", "ac", "에이에어컨"],
-    "tv": ["티비", "TV", "t v", "television"],
+    "air_conditioner": ["aircon", "ac", "airconditioner", "air_conditioner"],
+    "tv": ["tv", "TV", "television"],
 }
 
 ACTION_NORMALIZE = {
-    "power_on": ["on", "power_on", "power-on", "turn_on", "켜줘", "켜", "작동해"],
-    "power_off": ["off", "power_off", "power-off", "turn_off", "꺼줘", "꺼"],
-    "set_temperature": ["set_temp", "temperature", "set_temperature", "temp", "온도", "맞춰"],
+    "power_on": ["power", "on", "power_on", "turn_on", "turnon", "poweron"],
+    "power_off": ["off", "power_off", "turn_off", "turnoff", "poweroff"],
+    "set_temperature": ["set_temp", "settemp", "temperature", "set_temperature", "settemperature", "temp"],
 }
 
 CONTROL_TABLE = {
     "air_conditioner": {
-        "set_temp": set_aircon_temp,
+        "set_temperature": set_aircon_temp,
         "power_on": aircon_on,
         "power_off": aircon_off
     },
@@ -72,7 +72,7 @@ class ControlExectueNode(Node):
         device = self._normalize_device(parsed_result.get("device", "unknown"))
         command = self._normalize_command(parsed_result.get("command", "unknown"))
         value = parsed_result.get("value")
-
+        
         func = CONTROL_TABLE.get(device, {}).get(command)
         if func:
             func(value) if value else func()
@@ -85,8 +85,9 @@ class ControlExectueNode(Node):
             pub.data = "해당 디바이스를 제어할 수 없습니다."
             self.tts_request_publisher.publish(pub)
     
-    def _normalize_command(raw_action: str) -> str:
+    def _normalize_command(self, raw_action: str) -> str:
         raw = raw_action.lower().replace(" ", "").replace("-", "_")
+        print(raw)
 
         for standard, variants in ACTION_NORMALIZE.items():
             if raw in variants:
@@ -95,12 +96,25 @@ class ControlExectueNode(Node):
         # 못찾으면 예외 처리 또는 "unknown"
         return "unknown"
 
-    def _normalize_device(raw_device: str) -> str:
-        raw = raw_device.lower().replace(" ", "")
+    def _normalize_device(self, raw_device: str) -> str:
+        raw = raw_device.lower().replace(" ", "").replace("-", "_")
+        print(raw)
 
         for standard, variants in DEVICE_NORMALIZE.items():
             if raw in variants:
                 return standard
         return "unknown"
 
-    
+def main(args=None):
+    rclpy.init(args=args)
+    node = ControlExectueNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info('Keyboard Interrupt (SIGINT)')
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
